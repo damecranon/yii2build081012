2337573bffa30db9f24eeb9ad3334e8eed94c5ac
Damien Cranon <dame.cranon@gmail.com>
2015-07-21 00:03:44 -0400

    Changed the files

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,22 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
+    'modules' => [
+        'social' => [
+            // the module class
+            'class' => 'kartik\social\Module',
+            // the global settings for the disqus widget
+            'disqus' => [
+                'settings' => ['shortname' => 'DISQUS_SHORTNAME'] // default settings
+            ],
+            // the global settings for the facebook plugins widget
+            'facebook' => [
+                'appId' => '1466513327000490',
+                'secret' => '17211100151ee2c79e8e4e975178327d',
+],
+            ],
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
